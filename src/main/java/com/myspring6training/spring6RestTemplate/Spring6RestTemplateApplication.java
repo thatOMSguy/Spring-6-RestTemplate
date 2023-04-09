b27866dfb21a97416448015ce2287998f10545c8
Ravi Kumar <79511972+raviKumar-bsgi@users.noreply.github.com>
2023-04-09 20:54:06 +0530

    Initial Add Project setup

---
+++ src/main/java/com/myspring6training/spring6RestTemplate/Spring6RestTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.myspring6training.spring6RestTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6RestTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6RestTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myspring6training/spring6RestTemplate/Spring6RestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspring6training.spring6RestTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6RestTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
