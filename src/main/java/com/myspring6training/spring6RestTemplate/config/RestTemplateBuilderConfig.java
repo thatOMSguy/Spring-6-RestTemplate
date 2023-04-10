c8d1fe9b48c7c3923e104013073f59c4b929e83c
Ravi Kumar <79511972+raviKumar-bsgi@users.noreply.github.com>
2023-04-10 13:21:03 +0530

    Rest template config for base url

--- src/main/java/com/myspring6training/spring6RestTemplate/config/RestTemplateBuilderConfig.java
+++ src/main/java/com/myspring6training/spring6RestTemplate/config/RestTemplateBuilderConfig.java
@@ -1,6 +1,7 @@
 package com.myspring6training.spring6RestTemplate.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConfigurer;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -10,12 +11,15 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
 @Configuration
 public class RestTemplateBuilderConfig {
 
+    @Value("${rest.template.rootUrl}")
+    String rootUrl;
+
     @Bean
     RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer) {
         RestTemplateBuilder builder = configurer.configure(new RestTemplateBuilder());
 
         DefaultUriBuilderFactory uriBuilderFactory = new
-                DefaultUriBuilderFactory("http://localhost:8080");
+                DefaultUriBuilderFactory(rootUrl);
 
         return builder.uriTemplateHandler(uriBuilderFactory);
     }
