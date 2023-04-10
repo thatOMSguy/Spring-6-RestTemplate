a4c5dc915ee2142187a9c8fd5a2294435d5b4d5a
Ravi Kumar <79511972+raviKumar-bsgi@users.noreply.github.com>
2023-04-10 16:23:47 +0530

    BeerbyId added

--- src/main/java/com/myspring6training/spring6RestTemplate/client/BeerClient.java
+++ src/main/java/com/myspring6training/spring6RestTemplate/client/BeerClient.java
@@ -4,6 +4,8 @@ import com.myspring6training.spring6RestTemplate.model.BeerDTO;
 import com.myspring6training.spring6RestTemplate.model.BeerStyle;
 import org.springframework.data.domain.Page;
 
+import java.util.UUID;
+
 public interface BeerClient {
 
 
@@ -11,4 +13,8 @@ public interface BeerClient {
 
     Page<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory, Integer pageNumber,
                             Integer pageSize);
+
+    BeerDTO getBeerById(UUID id);
+
+    BeerDTO createNewBeer(BeerDTO newBeer);
 }
--- src/test/java/com/myspring6training/spring6RestTemplate/client/BeerClientImplTest.java
+++ src/test/java/com/myspring6training/spring6RestTemplate/client/BeerClientImplTest.java
@@ -1,8 +1,15 @@
 package com.myspring6training.spring6RestTemplate.client;
 
+import com.myspring6training.spring6RestTemplate.model.BeerDTO;
+import com.myspring6training.spring6RestTemplate.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class BeerClientImplTest {
@@ -10,6 +17,30 @@ class BeerClientImplTest {
 
     @Autowired
     BeerClientImpl beerClient;
+
+    @Test
+    void testCreateBeer() {
+        BeerDTO newBeer = BeerDTO.builder()
+                .price(new BigDecimal("10.89"))
+                .beerName("KF Ultra")
+                .beerStyle(BeerStyle.IPA)
+                .quantityOnHand(500)
+                .upc("85669789548").build();
+
+        BeerDTO savedBeer = beerClient.createNewBeer(newBeer);
+
+        assertNotNull(savedBeer);
+    }
+
+    @Test
+    void getBeerById() {
+        Page<BeerDTO> beerDTOS = beerClient.listBeers();
+        BeerDTO dto = beerDTOS.getContent().get(0);
+
+        BeerDTO beerById = beerClient.getBeerById(dto.getId());
+
+        assertNotNull(beerById);
+    }
 
     @Test
     void listBeersNoBeerName() {
