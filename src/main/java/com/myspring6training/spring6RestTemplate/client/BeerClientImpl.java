16b31f4c3054328db24b058679d2ecd2a9ff60e2
Ravi Kumar <79511972+raviKumar-bsgi@users.noreply.github.com>
2023-04-10 12:48:24 +0530

    getting list of beers as map earlier commit was for getting list as string

--- src/main/java/com/myspring6training/spring6RestTemplate/client/BeerClientImpl.java
+++ src/main/java/com/myspring6training/spring6RestTemplate/client/BeerClientImpl.java
@@ -8,20 +8,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 @RequiredArgsConstructor
 @Service
 public class BeerClientImpl implements BeerClient {
 
     private final RestTemplateBuilder restTemplateBuilder;
-
+    private static final String BASE_URL = "http://localhost:8080";
+    private static final String BEER_V1_PATH = "/api/v1/beer";
 
     @Override
     public Page<BeerDTO> listBeers() {
 
         RestTemplate restTemplate = restTemplateBuilder.build();
         ResponseEntity<String> stringResp =
-                restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
-        System.out.println(stringResp.getBody());
+                restTemplate.getForEntity(BASE_URL + BEER_V1_PATH, String.class);
+
+        ResponseEntity<Map> mapResp =
+                restTemplate.getForEntity(BASE_URL + BEER_V1_PATH, Map.class);
+
+        //System.out.println(stringResp.getBody());
+        System.out.println(mapResp.getBody().toString());
         return null;
     }
 }
