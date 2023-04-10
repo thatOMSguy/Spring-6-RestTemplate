a32c6fa80c24f35a40318177b736900b9db0a643
Ravi Kumar <79511972+raviKumar-bsgi@users.noreply.github.com>
2023-04-10 12:38:56 +0530

    added snippet to get beers while other spring app is running

--- src/main/java/com/myspring6training/spring6RestTemplate/client/BeerClientImpl.java
+++ src/main/java/com/myspring6training/spring6RestTemplate/client/BeerClientImpl.java
@@ -1,11 +1,27 @@
 package com.myspring6training.spring6RestTemplate.client;
 
 import com.myspring6training.spring6RestTemplate.model.BeerDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+@RequiredArgsConstructor
+@Service
 public class BeerClientImpl implements BeerClient {
+
+    private final RestTemplateBuilder restTemplateBuilder;
+
+
     @Override
     public Page<BeerDTO> listBeers() {
+
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        ResponseEntity<String> stringResp =
+                restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
+        System.out.println(stringResp.getBody());
         return null;
     }
 }
--- src/test/java/com/myspring6training/spring6RestTemplate/client/BeerClientImplTest.java
+++ src/test/java/com/myspring6training/spring6RestTemplate/client/BeerClientImplTest.java
@@ -1,12 +1,20 @@
 package com.myspring6training.spring6RestTemplate.client;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class BeerClientImplTest {
+
+
+    @Autowired
+    BeerClientImpl beerClient;
 
     @Test
     void listBeers() {
 
+        beerClient.listBeers();
     }
 
 }
