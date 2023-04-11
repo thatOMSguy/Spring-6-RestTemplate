833151d5c6281d24f18708a69c4dba9567eb5380
Ravi Kumar <79511972+raviKumar-bsgi@users.noreply.github.com>
2023-04-11 18:41:19 +0530

    config added for tests to run with basic auth for restmvc app running parallel in another wndow

--- src/main/java/com/myspring6training/spring6RestTemplate/config/RestTemplateBuilderConfig.java
+++ src/main/java/com/myspring6training/spring6RestTemplate/config/RestTemplateBuilderConfig.java
@@ -13,6 +13,11 @@ public class RestTemplateBuilderConfig {
 
     @Value("${rest.template.rootUrl}")
     String rootUrl;
+    @Value("${rest.template.username}")
+    String username;
+    @Value("${rest.template.password}")
+    String password;
+
 
     @Bean
     RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer) {
@@ -24,7 +29,9 @@ public class RestTemplateBuilderConfig {
         DefaultUriBuilderFactory uriBuilderFactory = new
                 DefaultUriBuilderFactory(rootUrl);
 
-        return builder.uriTemplateHandler(uriBuilderFactory);
+        RestTemplateBuilder builderWithAuth = builder.basicAuthentication("myuser", "password");
+
+        return builderWithAuth.uriTemplateHandler(uriBuilderFactory);
     }
 
 
